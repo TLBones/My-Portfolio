4a9de46ffa316a7700aad67a0e812b206f12ab0f
Ted <60035697+TLBones@users.noreply.github.com>
2020-06-29 19:17:01 -0600

    Intializing.
    Starting use of django templates. Using base design and information from current version of class portfolio.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .templates import *
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'portfolio/index.html')
